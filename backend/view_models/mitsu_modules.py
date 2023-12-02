3daf99fb922f4adec14f5032f0bd4b44be708b06
Fuitsuku <mitsulearnscode@gmail.com>
2023-12-02 09:21:03 -0700

    Created test methods for modules

--- backend/view_models/mitsu_modules.py
+++ backend/view_models/mitsu_modules.py
@@ -1,1 +1,12 @@
-from models import *
+import sys
+sys.path.append('/Users/mitsuakifukuzaki/Desktop/Hub/Programming/School/412/CSE412-SocialMediaSite/backend')
+from backend.models import Comment
+# from models.Tweet import Tweet
+# from models.Comment import Comment
+
+def createUser():
+    cmmnt = Comment( 'usename1', 'username2', 1, '1', 'test')
+
+    print(cmmnt.commenting_username)
+
+createUser()
