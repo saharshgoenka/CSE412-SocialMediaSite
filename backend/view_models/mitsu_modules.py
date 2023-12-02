6b75331348ee32dd34ad6822dd6d0b6b51079011
Fuitsuku <mitsulearnscode@gmail.com>
2023-12-02 09:38:32 -0700

    Started loadUsersComments()

--- backend/view_models/mitsu_modules.py
+++ backend/view_models/mitsu_modules.py
@@ -1,12 +1,33 @@
-import sys
-# sys.path.append('/Users/mitsuakifukuzaki/Desktop/Hub/Programming/School/412/CSE412-SocialMediaSite/backend')
 from models import Comment
 from models.Tweet import Tweet
 from models.Comment import Comment
+import psycopg2
+
+def create_db_connection():
+	connection = psycopg2.connect(
+        user='mitsuakifukuzaki',
+		host="/tmp",
+		port="5432",
+		database="mitsuakifukuzaki"
+	)
+	return connection
 
 def createUser():
     cmmnt = Comment( 'usename1', 'username2', 1, '1', 'test')
 
     print(cmmnt.commenting_username)
 
-createUser()
+
+def loadUsersComments(commenting_username:str):
+    connection = create_db_connection()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM Cmmnt WHERE commenting_username = %s", (commenting_username, ))
+        comments = cursor.fetchall()
+
+        print(comments)
+    except psycopg2.Error as e:
+        print("Error:", e)
+
+loadUsersComments('username1')
