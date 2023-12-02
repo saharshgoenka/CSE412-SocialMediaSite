7fba25e6bf1d78457949d9141a858ec6fd4ff5e8
Fuitsuku <mitsulearnscode@gmail.com>
2023-12-02 09:25:47 -0700

    Changed models location for user usability

--- backend/view_models/mitsu_modules.py
+++ backend/view_models/mitsu_modules.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append('/Users/mitsuakifukuzaki/Desktop/Hub/Programming/School/412/CSE412-SocialMediaSite/backend')
-from backend.models import Comment
-# from models.Tweet import Tweet
-# from models.Comment import Comment
+# sys.path.append('/Users/mitsuakifukuzaki/Desktop/Hub/Programming/School/412/CSE412-SocialMediaSite/backend')
+from models import Comment
+from models.Tweet import Tweet
+from models.Comment import Comment
 
 def createUser():
     cmmnt = Comment( 'usename1', 'username2', 1, '1', 'test')
