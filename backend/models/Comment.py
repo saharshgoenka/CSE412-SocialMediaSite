87b7a24a287479231afc4bb2476d2869f67eeb50
Fuitsuku <mitsulearnscode@gmail.com>
2023-12-02 08:52:26 -0700

    Created a constructor to Comment.py

--- backend/models/Comment.py
+++ backend/models/Comment.py
@@ -4,3 +4,11 @@ class Comment:
     tweet_id:int
     timestmp:str
     comment_content:str
+
+    def __init__(self, commenting_username:str, tweeting_username:str, tweet_id:int, timestmp:str, comment_content:str):
+        self.commenting_username = commenting_username
+        self.tweeting_username = tweeting_username
+        self.tweet_id = tweet_id
+        self.timestmp = timestmp
+        self.comment_content = comment_content
+        
