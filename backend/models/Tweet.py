7b97b02d355e74d46501a08f6c4e811b6f576057
aditprbhu <adprabh2@asu.edu>
2023-12-02 08:59:03 -0700

    Tweet model complete

--- backend/models/Tweet.py
+++ backend/models/Tweet.py
@@ -2,7 +2,7 @@
 Tweet Class: Used to store tweet information
 """
 
-class User:
+class Tweet:
     def __init__(self, original_username: str, tweet_id: int, cntnt: str, likes: int, reshares: int, timestmp:str):
         self.original_username = original_username
         self.tweet_id = tweet_id
