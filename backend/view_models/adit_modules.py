92ef8d3ed39415061c5adfc6841ebd092ee0e07b
aditprbhu <adprabh2@asu.edu>
2023-12-02 09:20:26 -0700

    backend works?

--- backend/view_models/adit_modules.py
+++ backend/view_models/adit_modules.py
@@ -1,1 +1,23 @@
-from models import *
+import sys
+sys.path.append('/Users/enigma/Desktop/ASU2023/Fall2023/CSE412/FinalProject/CSE412-SocialMediaSite/backend')
+
+from models.User import User
+from models.Tweet import Tweet
+from models.Comment import Comment
+
+user2 = User(
+        "jane_doe",
+        "strong_password",
+        "2023-02-15",
+        "jane.doe@example.com",
+        "Jane Doe",
+        "jane_profile.jpg",
+        "1985-05-20"
+    )
+
+# Accessing attributes
+print(user2.username)
+print(user2.email)
+
+# Print the user object
+print(user2)
