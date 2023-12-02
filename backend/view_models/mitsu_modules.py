c3b7a2a6dfbe562483f2599eb73188310264efc7
Fuitsuku <mitsulearnscode@gmail.com>
2023-12-02 11:58:05 -0700

    Commented Methods

--- backend/view_models/mitsu_modules.py
+++ backend/view_models/mitsu_modules.py
@@ -13,15 +13,21 @@ def create_db_connection():
 	)
 	return connection
 
+# Loads a specified user's tweets and returns an array of Tweet Objects
 def loadUserTweets(original_username:str):
     connection = create_db_connection()
     cursor = connection.cursor()
 
     try:
+        # Query all tweets associated with a user
         cursor.execute("SELECT * FROM Tweet WHERE original_username = %s", (original_username, ))
         tweets = cursor.fetchall()
 
+        # Iterate through the tweets and create Tweet Objects for each
         tweet_list = []
+
+        # Each Tweet in Tweets is in the following format 
+        # (original_username, tweet_ID, cntnt, likes, reshares, timestmp)
         for tweet in tweets:
             original_username_temp = tweet[0],
             tweet_ID_temp          = tweet[1],
@@ -30,6 +36,7 @@ def loadUserTweets(original_username:str):
             reshares_temp          = tweet[4],
             timestmp_temp          = tweet[5]
 
+            # Create a new Tweet Object 
             new_tweet = Tweet(
                                 original_username= original_username_temp,
                                 tweet_id         = tweet_ID_temp,
@@ -39,9 +46,13 @@ def loadUserTweets(original_username:str):
                                 timestmp         = timestmp_temp
                              )
             
+            # Insert the newly created Tweet into the tweet_list
             tweet_list.append(new_tweet)
 
+        # Used to confirm the correct number of tweets were inserted into the array
         print(len(tweet_list))
+
+        # Return the array
         return tweet_list
             
     except psycopg2.Error as e:
@@ -56,9 +67,12 @@ def loadTweet(original_username:str, tweet_ID:int):
     cursor = connection.cursor()
 
     try:
+
+        # Load tweet under specified parameter
         cursor.execute("SELECT * FROM Tweet WHERE original_username = %s AND tweet_id = %s", (original_username, tweet_ID))
         tweet_intermediate = cursor.fetchall()
-
+        
+        # Convert to Tweet Object
         tweet = Tweet(
             original_username=tweet_intermediate[0][0],
             tweet_id=tweet_intermediate[0][1],
@@ -68,12 +82,13 @@ def loadTweet(original_username:str, tweet_ID:int):
             timestmp=tweet_intermediate[0][5]
         )
         
+        # Return Tweet object
         return tweet
     
     except psycopg2.Error as e:
         print("Error:", e)
     finally:
-        connection.commit();
+        connection.commit()
         cursor.close()
         connection.close()
 
@@ -82,15 +97,18 @@ def createComment(commenting_username:str, tweeting_username:str, tweet_ID:int, 
     cursor = connection.cursor()
 
     try:
+        # Create current time stamp
         timestmp_intermediate = datetime.now()
         timestmp = timestmp_intermediate.strftime("%Y-%m-%d %H:%M:%S")
 
-        cursor.execute("INSERT INTO Cmmnt(commenting_username, tweeting_username, tweet_id, timestmp, comment_content) VALUES (%s, %s, %s, %s, %s);", (commenting_username, tweeting_username, tweet_ID, timestmp, comment_content))
+        # Insert the new comment into DB
+        cursor.execute("INSERT INTO Cmmnt(commenting_username, tweeting_username, tweet_id, timestmp, comment_content) VALUES (%s, %s, %s, %s, %s)", (commenting_username, tweeting_username, tweet_ID, timestmp, comment_content))
     
     except psycopg2.Error as e:
         print("Error:", e)
     finally:
-        connection.commit();
+        # Commit changes
+        connection.commit()
         cursor.close()
         connection.close()
 
@@ -99,12 +117,14 @@ def editComment(commenting_username:str, tweeting_username:str, tweet_ID:int, ti
     cursor = connection.cursor()
 
     try:
+        # Edit Comments
         cursor.execute("UPDATE Cmmnt SET comment_content = %s WHERE commenting_username = %s AND tweeting_username = %s AND tweet_id = %s AND timestmp = %s", (new_content, commenting_username, tweeting_username, tweet_ID, timestamp))
     
     except psycopg2.Error as e:
         print("Error:", e)
     finally:
-        connection.commit();
+        # Commit Changes
+        connection.commit()
         cursor.close()
         connection.close()
 
@@ -113,12 +133,14 @@ def deleteComment(commenting_username:str, tweeting_username:str, tweet_ID:int, 
     cursor = connection.cursor()
 
     try:
+        # Delete Tweet
         cursor.execute("DELETE FROM Cmmnt WHERE commenting_username = %s AND tweeting_username = %s AND tweet_id = %s AND timestmp = %s", (commenting_username, tweeting_username, tweet_ID, timestamp))
     
     except psycopg2.Error as e:
         print("Error:", e)
     finally:
-        connection.commit();
+        # Commit Changes
+        connection.commit()
         cursor.close()
         connection.close()
 
@@ -127,9 +149,11 @@ def loadUserComments(commenting_username:str):
     cursor = connection.cursor()
 
     try:
+        # Load all tweets
         cursor.execute("SELECT * FROM Cmmnt WHERE commenting_username = %s", (commenting_username, ))
         comments = cursor.fetchall()
 
+        # Iterate through the comments and create Comment Objects
         comment_list = []
         for comment in comments:
             commenting_username_temp = comment[0]
@@ -138,6 +162,7 @@ def loadUserComments(commenting_username:str):
             timestmp_temp            = comment[3]
             comment_content_temp     = comment[4]
 
+            # Convert the current comment into a Comment Object
             new_comment = Comment(
                                     commenting_username= commenting_username_temp,
                                     tweeting_username  = tweeting_username_temp,
@@ -146,9 +171,13 @@ def loadUserComments(commenting_username:str):
                                     comment_content    = comment_content_temp
                                 )
             
+            # Add the new Comment Object to the array
             comment_list.append(new_comment)
 
+        # Used to confirm the correct number of comments loaded
         print(len(comment_list))
+
+        # return the list of comments
         return comment_list
             
     except psycopg2.Error as e:
